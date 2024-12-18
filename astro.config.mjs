73bbf9ccd68d184a90ebc3887f482c29898e6541
lafitteque <quentin.laf@gmail.com>
2024-12-18 16:44:39 +0100

    tests

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://lafitteque.github.io',
-  base: '/lafitteque.github.io/',  // Si ton dépôt est sous ce nom
+  base: '',  // Si ton dépôt est sous ce nom
 });
