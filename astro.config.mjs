c5612bdf8fd246fa85bd275fd5241ca047299b55
lafitteque <quentin.laf@gmail.com>
2024-12-19 19:40:43 +0100

    Working local

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
-
+import mdx from '@astrojs/mdx';
 export default defineConfig({
+  build: {
+    format: 'directory', // Crée une structure de fichiers et dossiers
+  },
   site: 'https://lafitteque.github.io',
-  base: '',  // Si ton dépôt est sous ce nom
+  base: '', 
+  integrations: [mdx()],
 });
